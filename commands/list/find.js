b4350884214fd20776f015b83a4e684ff77a2682
Loïc <lbertran@student.42lyon.fr>
2021-10-13 12:34:19 +0200

    quick push

--- commands/list/find.js
+++ commands/list/find.js
@@ -1,26 +1,15 @@
-const SerpWow = require('google-search-results-serpwow');
-
-let serpwow = new SerpWow('CCA26AADDB2E492B8EA601FDF04E9E43');
+const google = require('googlethis');
 
 const execute = function(msg, to_find) {
     msg.channel.send(`Searching for [${to_find}]...`);
 	to_find += ' site:myanimelist.net'
 	console.log(to_find);
-	serpwow.json(
-		{
-		  q: to_find,
-		  num: 1,
-		  location: 'Paris,Paris,Ile-de-France,France'
-		})
-		.then(result => {
-		  // pretty-print the result
-		  console.log(JSON.stringify(result, 0, 2));
-		  console.log(result.organic_results[0].link);
-		})
-		.catch(error => {
-		  // print the error
-		  console.log(error);
-		});
+	const options = {
+		page: 0, 
+		safe: false, // hide explicit results?
+	}
+	const response = await google.search("TWDG", options);
+  	console.log(response);
 }
 
 module.exports = {
