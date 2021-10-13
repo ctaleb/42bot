df0762528b6c74eac8d6a7cba22c0d2e71611553
Loïc <lbertran@student.42lyon.fr>
2021-10-13 12:27:39 +0200

    quick push

--- commands/list/find.js
+++ commands/list/find.js
@@ -6,7 +6,7 @@ const execute = function(msg, to_find) {
     msg.channel.send(`Searching for [${to_find}]...`);
 	to_find += ' site:myanimelist.net'
 	console.log(to_find);
-	var json = serpwow.json(
+	serpwow.json(
 		{
 		  q: to_find,
 		  num: 1,
@@ -15,12 +15,12 @@ const execute = function(msg, to_find) {
 		.then(result => {
 		  // pretty-print the result
 		  console.log(JSON.stringify(result, 0, 2));
+		  console.log(result.organic_results[0].link);
 		})
 		.catch(error => {
 		  // print the error
 		  console.log(error);
 		});
-	console.log(json.link);
 }
 
 module.exports = {
