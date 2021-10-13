d5052c1ae8da8eb5d208a98550b367500b2ff23b
Loïc <lbertran@student.42lyon.fr>
2021-10-13 11:30:17 +0200

    quick push

--- commands/list/find.js
+++ commands/list/find.js
@@ -4,7 +4,7 @@ google.resultsPerPage = 25;
 
 const execute = function(msg, to_find) {
     msg.channel.send(`Searching for [${to_find}]...`);
-	to_find += ' site:myanimelist.net'
+	//to_find += ' site:myanimelist.net'
 	console.log(to_find);
 	google(to_find , function (err, res){
 		if (err) console.error(err)
